e8768bfd29d589417e6ead1e1042c8aeaa6317f6
Jason Fong <jasonlmfong@gmail.com>
2024-04-23 20:52:33 -0400

    use vite

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
+import App from "./App.tsx";
 
 ReactDOM.render(
     <React.StrictMode>
