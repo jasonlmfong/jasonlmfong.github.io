508aac67dbefffe4c80150efde1b76639e482089
jasonlmfong <jasonlmfong@gmail.com>
2025-06-13 15:04:35 -0400

    allow style override

--- src/components/PageSection.tsx
+++ src/components/PageSection.tsx
@@ -3,7 +3,7 @@ import Row from "react-bootstrap/Row";
 export default function PageSection(props) {
     return (
         <Row {...props} className={"justify-content-md-center " + props.className}
-             style={{...props.style, minHeight: "calc(100vh - 56px)", padding: "25px 0 25px 0"}}>
+             style={{minHeight: "30vh", padding: "25px 0 25px 0", ...props.style}}>
             {props.children}
         </Row>
     );
